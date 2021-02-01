d1a9587980aab8e180a5251b1196d483dcb489a4
Eve <53002304+omwfr019@users.noreply.github.com>
2021-02-02 01:48:23 +0900

    Create Counting_Valleys.java

---
+++ study_Java/HackerRank/Counting_Valleys.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, String path) {
+        int result = 0;
+        int sum = 0;
+        int preSum = 0;
+        
+        for(int inputIdx = 0; inputIdx < steps; inputIdx++){
+            if (path.charAt(inputIdx)=='U') sum += 1;
+            if (path.charAt(inputIdx)=='D') sum -= 1;
+            
+            if (sum == 0 && preSum <0){
+                result++;
+            }
+            
+            preSum = sum;
+        }
+        
+        return result;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int steps = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String path = bufferedReader.readLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
