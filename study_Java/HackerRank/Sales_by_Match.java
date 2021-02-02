c843387e2eff45a07fc1115212808ec140398250
Eve <53002304+omwfr019@users.noreply.github.com>
2021-02-03 00:47:25 +0900

    Update Sales_by_Match.java

--- study_Java/HackerRank/Sales_by_Match.java
+++ study_Java/HackerRank/Sales_by_Match.java
@@ -2,6 +2,7 @@
 
 There is a large pile of socks that must be paired by color. 
 Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+(https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup)
 
 */
 
